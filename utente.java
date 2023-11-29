babc735764e9ee1cb25cdb45c44bc938e513f7f8
WiserGG <90522347+WiserGG@users.noreply.github.com>
2023-11-29 19:57:12 +0100

    aggiunta metodi GET e toString con token "-"

--- utente.java
+++ utente.java
@@ -1,17 +1,38 @@
 /* questa classe salva il nome utente, dopo conferma del server.
  * Le sue funzioni sono:
- * salvare il nome utente ricevuto tramite costruttore
- * restituire il nome utente che potrebbe servire per eseguire confronti con altri nome utente
+ * salvare le informazioni dell'utente che potrebbero essere usate per effettuare il login automatico o non
+ * se la variabile booleana ricordaPassword è true non dovrà inserirla per effettuare l'accesso, dovrà utilizzare
+ * solamente l'username che è univoco per ogni utente 
 */
 
 public class utente {
     private String username;
+    private String password;
+    private boolean ricordaPassword;
 
-    public utente(String username) {
+    //costruttore utilizzato per inviare i dati al server o per memorizzare l'utente al server
+    public utente(String username, String password, boolean ricordaPassword){
         this.username = username;
+        this.password = password;
+        this.ricordaPassword = ricordaPassword;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public boolean getRicordaPassword(){
+        return ricordaPassword;
+    }
+
+
+    //utilizzato dal server per effettuare lavor
+    @Override
+    public String toString() {
+        return username + "-" + password + "-" + ricordaPassword;
     }
 }
